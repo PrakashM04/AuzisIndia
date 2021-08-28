7b8a21991e8c96b025d0f0312009710185840ccb
PrakashM04 <89623775+PrakashM04@users.noreply.github.com>
2021-08-29 04:28:51 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,14 @@ document.querySelector('#menu-btn').onclick = () => {
     searchForm.classList.remove('active');
 }
 
+let contact = document.querySelector('.contact');
+
+document.querySelector('#contact-btn').onclick = () => {
+    contact.classList.toggle('active');
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
 let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () => {
@@ -15,4 +23,5 @@ document.querySelector('#search-btn').onclick = () => {
 window.onscroll = () => {
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
+    contact.classList.remove('active');
 }
